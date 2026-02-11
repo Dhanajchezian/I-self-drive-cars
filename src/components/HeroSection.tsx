64c777e96122ec590bbe20d269c3ea1f3c7b5eb5
SHAIKAFNAN11 <shaikmohammedafnan1701@gmail.com>
2026-02-11 21:41:24 +0530

    button

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -44,12 +44,6 @@ const HeroSection = () => {
         </div>
       </div>
 
-      {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-float">
-        <div className="w-6 h-10 border-2 border-primary-foreground/30 rounded-full flex items-start justify-center pt-2">
-          <div className="w-1.5 h-3 bg-primary-foreground/50 rounded-full" />
-        </div>
-      </div>
     </section>
   );
 };
