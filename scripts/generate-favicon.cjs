f0ded2690deb20412a787ce2ee119882bb50cb65
SHAIKAFNAN11 <shaikmohammedafnan1701@gmail.com>
2026-02-11 21:28:15 +0530

    icon

---
+++ scripts/generate-favicon.cjs
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
+const pngToIco = require('png-to-ico').default || require('png-to-ico');
+
+(async () => {
+  try {
+    const svgPath = path.join(__dirname, '..', 'public', 'placeholder.svg');
+    const outPath = path.join(__dirname, '..', 'public', 'favicon.ico');
+    const sizes = [16, 32, 48, 64, 128];
+
+    const pngBuffers = await Promise.all(
+      sizes.map(size => sharp(svgPath).resize(size, size).png().toBuffer())
+    );
+
+    const icoBuffer = await pngToIco(pngBuffers);
+    fs.writeFileSync(outPath, icoBuffer);
+    console.log('Created:', outPath);
+  } catch (err) {
+    console.error('Error creating favicon.ico:', err);
+    process.exit(1);
+  }
+})();
